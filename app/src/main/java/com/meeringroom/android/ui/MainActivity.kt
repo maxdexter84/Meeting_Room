fa64ca9cc309fdaf6b933c7eb0ed283a53b3b184
VasiaLelekaAndersen <v.leleka@andersenlab.com>
2021-07-08 17:12:42 +0300

    init commit

---
+++ app/src/main/java/com/meeringroom/android/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.meeringroom.android.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.meeringroom.android.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
