2f0245e243fd3d71ca402d8e52b365e01802276d
чорный максим <polazaz1111@gmail.com>
2021-07-23 11:19:40 +0300

    added builder

--- app/src/main/java/com/meetingroom/android/ApplicationMeetingRoom.kt
+++ app/src/main/java/com/meetingroom/android/ApplicationMeetingRoom.kt
@@ -2,9 +2,16 @@ package com.meetingroom.android
 
 import android.app.Application
 import com.meetingroom.android.di.ApplicationComponent
+import com.meetingroom.android.di.ApplicationModule
 import com.meetingroom.android.di.DaggerApplicationComponent
 
 class ApplicationMeetingRoom : Application() {
-    val appComponent: ApplicationComponent = DaggerApplicationComponent.create()
+    private lateinit var appComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent =
+            DaggerApplicationComponent.builder().applicationModule(ApplicationModule(this)).build()
+    }
 
 }
