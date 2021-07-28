40347d983d4d74ba7f7bede51037f20ff8645d80
Vladimir Kuznetsov <flyman335@gmail.com>
2021-07-28 09:56:32 +0300

    Update interface for login fragment

--- feature_login/src/main/java/com/meetingroom/feature_login/LoginFragment.kt
+++ feature_login/src/main/java/com/meetingroom/feature_login/LoginFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.meetingroom.feature_login.databinding.LoginFragmentBinding
 import com.meetingroom.feature_login.di.DaggerLoginComponent
@@ -38,5 +39,17 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        (requireActivity() as AppCompatActivity).supportActionBar?.hide()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        (requireActivity() as AppCompatActivity).supportActionBar?.show()
     }
 }
