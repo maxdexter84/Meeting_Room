29b31277a9fa38a70df79f2964286bcae3b8862e
Vladimir Kuznetsov <flyman335@gmail.com>
2021-07-29 14:40:46 +0300

    Clear wrong imports

--- app/src/main/java/com/meetingroom/android/ui/MainActivity.kt
+++ app/src/main/java/com/meetingroom/android/ui/MainActivity.kt
@@ -3,8 +3,6 @@ package com.meetingroom.android.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.meetingroom.android.R
-import com.meetingroom.android.sharedpreferences.SharedPreferencesHelper
-import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
