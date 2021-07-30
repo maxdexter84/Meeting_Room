fc8f5976e5139c00bedc34ff9a0910f27c649104
чорный максим <polazaz1111@gmail.com>
2021-07-30 15:04:37 +0300

    changed call to Response

--- core_network/src/main/java/com/example/core_network/user_interfaces/LogInInterface.kt
+++ core_network/src/main/java/com/example/core_network/user_interfaces/LogInInterface.kt
@@ -2,11 +2,11 @@ package com.example.core_network.user_interfaces
 
 import com.example.core_network.user_posts.LogInPost
 import com.example.core_network.user_responses.LogInResponse
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface LogInInterface {
     @POST("signin")
-    fun logInUser(@Body post: LogInPost): Call<LogInResponse>
+    suspend fun logInUser(@Body post: LogInPost): Response<LogInResponse>
 }
