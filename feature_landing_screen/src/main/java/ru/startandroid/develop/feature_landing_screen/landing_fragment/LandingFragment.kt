4677d492441177aff861234a1ecb2319f48e5584
Baudi2 <60889423+Baudi2@users.noreply.github.com>
2021-08-30 17:35:48 +0300

    Done wiring up toolbar with logout logic in new module

--- feature_landing_screen/src/main/java/ru/startandroid/develop/feature_landing_screen/landing_fragment/LandingFragment.kt
+++ feature_landing_screen/src/main/java/ru/startandroid/develop/feature_landing_screen/landing_fragment/LandingFragment.kt
@@ -2,9 +2,15 @@ package ru.startandroid.develop.feature_landing_screen.landing_fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import android.view.View
+import androidx.appcompat.widget.PopupMenu
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.MenuCompat
 import androidx.fragment.app.Fragment
 import com.example.core_module.sharedpreferences_di.SharedPreferencesModule
+import com.meeringroom.ui.view.toolbar.ToolbarHandlerOptions
 import ru.startandroid.develop.feature_landing_screen.R
 import ru.startandroid.develop.feature_landing_screen.databinding.FragmentLandingBinding
 import ru.startandroid.develop.feature_landing_screen.di.DaggerLandingFragmentComponent
@@ -14,6 +20,7 @@ import javax.inject.Inject
 class LandingFragment : Fragment(R.layout.fragment_landing) {
 
     private lateinit var binding: FragmentLandingBinding
+
     @Inject
     lateinit var viewModel: LandingFragmentViewModel
 
@@ -31,12 +38,50 @@ class LandingFragment : Fragment(R.layout.fragment_landing) {
         binding = FragmentLandingBinding.bind(view)
 
         with(binding) {
-
+            landingToolbar.setToolBarTitle(getString(R.string.toolbar_landing_title))
+            landingToolbar.changeToolBarConfiguration(
+                ToolbarHandlerOptions.More(
+                    onIconClick = { showPopUpMenu(landingToolbar.requireIconAsView()) }
+                )
+            )
         }
     }
 
     override fun onStart() {
         super.onStart()
         viewModel.isDeleteRequired()
+    }
+
+    private fun showPopUpMenu(view: View) {
+        val popupMenu = PopupMenu(requireContext(), view)
+        popupMenu.inflate(R.menu.base_menu)
+        MenuCompat.setGroupDividerEnabled(popupMenu.menu, true)
+        popupMenu.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.location_settings -> true
+                R.id.theme_colors -> true
+                R.id.log_out -> {
+                    viewModel.logout()
+                    true
+                }
+                else -> false
+            }
+        }
+        for (i in 2 until popupMenu.menu.size()) {
+            val menuItem = popupMenu.menu.getItem(i)
+            val spannable =
+                SpannableString(popupMenu.menu.getItem(i).title.toString())
+            spannable.setSpan(
+                ForegroundColorSpan(
+                    ResourcesCompat.getColor(
+                        resources,
+                        R.color.red_for_logout_text,
+                        null
+                    )
+                ), 0, spannable.length, 0
+            )
+            menuItem.title = spannable
+        }
+        popupMenu.show()
     }
 }
