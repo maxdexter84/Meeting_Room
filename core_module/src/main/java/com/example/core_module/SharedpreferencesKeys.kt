3651d27cf102e5b31829f72dd4eb42a0bc7057ea
чорный максим <polazaz1111@gmail.com>
2021-09-07 15:43:04 +0300

    added consts

---
+++ core_module/src/main/java/com/example/core_module/SharedpreferencesKeys.kt
@@ -0,0 +1,9 @@
+package com.example.core_module
+
+object SharedpreferencesKeys {
+    const val TOKEN_DAY_KEY = "token_day"
+    const val ACCESS_TOKEN_KEY = "token"
+    const val CITY_OF_USER_LOCATION_KEY = "city"
+    const val COUNTRY_OF_USER_LOCATION_KEY = "country"
+    const val USER_ROLES_KEY = "user_roles"
+}
