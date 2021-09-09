f0c76dd783d4b199e15a419c0f53080a8e3e5f87
Baudi2 <60889423+Baudi2@users.noreply.github.com>
2021-09-09 12:32:05 +0300

    Can't extract extras with strings

--- feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/upcoming_events_fragment/presentation/BroadcastForUpcomingEvent.kt
+++ feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/upcoming_events_fragment/presentation/BroadcastForUpcomingEvent.kt
@@ -3,10 +3,28 @@ package com.meetingroom.andersen.feature_landing.upcoming_events_fragment.presen
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.meetingroom.andersen.feature_landing.R
 
-class BroadcastForUpcomingEvent: BroadcastReceiver() {
+class BroadcastForUpcomingEvent : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        Toast.makeText(context, "Yo!", Toast.LENGTH_SHORT).show()
+        if (context == null) return
+        val notification = NotificationCompat.Builder(
+            context,
+            NotificationHelper.REMINDER_NOTIFICATION_CHANNEL
+        )
+            .setSmallIcon(R.drawable.ic_small_andersen_icon_notification)
+            .setContentTitle(intent?.getStringExtra(NotificationHelper.REMINDER_NOTIFICATION_TITLE))
+            .setPriority(NotificationCompat.PRIORITY_MAX)
+            .setCategory(NotificationCompat.CATEGORY_REMINDER)
+            .setStyle(
+                NotificationCompat.BigTextStyle()
+                    .bigText(intent?.getStringExtra(NotificationHelper.REMINDER_NOTIFICATION_DESCRIPTION))
+            )
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(NotificationHelper.UPCOMING_EVENT_NOTIFICATION_ID, notification)
     }
 }
--- feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/upcoming_events_fragment/presentation/NotificationHelper.kt
+++ feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/upcoming_events_fragment/presentation/NotificationHelper.kt
@@ -5,8 +5,6 @@ import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.meetingroom.andersen.feature_landing.R
 import com.meetingroom.andersen.feature_landing.upcoming_events_fragment.model.UpcomingEventData
 import kotlinx.datetime.Clock
@@ -31,37 +29,26 @@ class NotificationHelper @Inject internal constructor(private val context: Conte
     }
 
     fun setNotification(upcomingEventData: UpcomingEventData) {
-        scheduleNotification(buildNotification(upcomingEventData))
-    }
-
-    private fun buildNotification(
-        upcomingEventData: UpcomingEventData
-    ): Notification {
-        return NotificationCompat.Builder(context, REMINDER_NOTIFICATION_CHANNEL)
-            .setSmallIcon(R.drawable.ic_small_andersen_icon_notification)
-            .setContentTitle(upcomingEventData.title)
-            .setPriority(NotificationCompat.PRIORITY_MAX)
-            .setCategory(NotificationCompat.CATEGORY_REMINDER)
-            .setStyle(
-                NotificationCompat.BigTextStyle()
-                    .bigText(
-                        String.format(
-                            context.resources.getString(R.string.event_upcoming_notification_description),
-                            upcomingEventData.eventRoom,
-                            upcomingEventData.startTime,
-                            upcomingEventData.reminderRemainingTime,
-                        )
-                    )
-            )
-            .build()
+        scheduleNotification(upcomingEventData)
     }
 
     @SuppressLint("UnspecifiedImmutableFlag")
-    private fun scheduleNotification(notification: Notification) {
+    private fun scheduleNotification(upcomingEventData: UpcomingEventData) {
+        val title = upcomingEventData.title
+        val notificationDescription =
+            String.format(
+                context.resources.getString(R.string.event_upcoming_notification_description),
+                upcomingEventData.eventRoom,
+                upcomingEventData.startTime,
+                upcomingEventData.reminderRemainingTime,
+            )
         val currentTime = Clock.System.now().toEpochMilliseconds()
         val notificationScheduledTime = 200 * 10
+        val intent = Intent(context, BroadcastForUpcomingEvent::class.java)
+        intent.putExtra(REMINDER_NOTIFICATION_TITLE, title)
+        intent.putExtra(REMINDER_NOTIFICATION_DESCRIPTION, notificationDescription)
         val pendingIntent = PendingIntent.getBroadcast(
-            context, 0, Intent(context, BroadcastForUpcomingEvent::class.java), 0
+            context, 0, intent, 0
         )
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as? AlarmManager
         alarmManager?.set(
@@ -69,11 +56,11 @@ class NotificationHelper @Inject internal constructor(private val context: Conte
             currentTime + notificationScheduledTime,
             pendingIntent
         )
-        val notificationManager = NotificationManagerCompat.from(context)
-        notificationManager.notify(UPCOMING_EVENT_NOTIFICATION_ID, notification)
     }
 
     companion object {
+        const val REMINDER_NOTIFICATION_DESCRIPTION = "description"
+        const val REMINDER_NOTIFICATION_TITLE = "title"
         const val REMINDER_NOTIFICATION_CHANNEL = "REMINDER_NOTIFICATION_CHANNEL"
         const val UPCOMING_EVENT_NOTIFICATION_ID = 1
     }
