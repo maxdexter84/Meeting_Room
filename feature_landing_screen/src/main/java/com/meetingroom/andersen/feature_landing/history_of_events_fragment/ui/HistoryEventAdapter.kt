5dc833c91917db8226d0989cc79b442b30623fda
чорный максим <polazaz1111@gmail.com>
2021-09-09 11:24:23 +0300

    added recycler and gag for history events

---
+++ feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/history_of_events_fragment/ui/HistoryEventAdapter.kt
@@ -0,0 +1,99 @@
+package com.meetingroom.andersen.feature_landing.history_of_events_fragment.ui
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.meeringroom.ui.view_utils.visibilityIf
+import com.meetingroom.andersen.feature_landing.R
+import com.meetingroom.andersen.feature_landing.databinding.EventElementHistoryBinding
+import com.meetingroom.andersen.feature_landing.history_of_events_fragment.model.HistoryEventData
+
+
+class HistoryEventAdapter : RecyclerView.Adapter<HistoryEventAdapter.HistoryEventViewHolder>() {
+    private val events = ArrayList<HistoryEventData>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HistoryEventViewHolder {
+        return HistoryEventViewHolder(
+            EventElementHistoryBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(
+        holder: HistoryEventViewHolder,
+        position: Int
+    ) {
+        holder.bind(events[position])
+    }
+
+    override fun getItemCount() = events.size
+
+    fun setData(newEvents: List<HistoryEventData>) {
+        val diffCallback = HistoryEventsDiffCallback(events, newEvents)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
+        events.clear()
+        events.addAll(newEvents)
+        diffResult.dispatchUpdatesTo(this)
+    }
+
+    class HistoryEventViewHolder(private val binding: EventElementHistoryBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(historyEventData: HistoryEventData) {
+            with(binding) {
+                eventTitleUpcoming.text = historyEventData.title
+                eventPlannedTimeUpcoming.text =
+                    String.format(
+                        binding.root.resources.getString(R.string.event_planned_time_upcoming),
+                        historyEventData.startTime,
+                        historyEventData.endTime
+                    )
+                eventPlannedDateUpcoming.text = historyEventData.eventDate
+                eventRoomUpcoming.text = historyEventData.eventRoom
+                eventCityColourLineUpcoming.setBackgroundResource(historyEventData.eventRoomColour)
+                eventCardUpcomingRoot.setOnLongClickListener {
+                    flexiblePartOfCardView.visibilityIf(!flexiblePartOfCardView.isVisible)
+                    nameOfBooker.text = historyEventData.bookerName
+                    roleOfBooker.text = historyEventData.bookerPosition
+                    bookerEmail.text = historyEventData.bookerEmail
+                    bookerSkype.text = historyEventData.bookerSkype
+                    descriptonOfEvent.text = historyEventData.description
+                    qwerty(eventTitleUpcoming.text.toString())
+                }
+            }
+
+        }
+
+        private fun qwerty(string: String): Boolean {
+            Log.e("forMax", string)
+            return true
+        }
+    }
+
+    class HistoryEventsDiffCallback(
+        private val oldList: List<HistoryEventData>,
+        private val newList: List<HistoryEventData>
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize() = oldList.size
+
+        override fun getNewListSize() = newList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition].title == newList[newItemPosition].title
+        }
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+            return oldList[oldItemPosition] == newList[newItemPosition]
+        }
+    }
+}
--- feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/history_of_events_fragment/ui/HistoryOfEventsFragment.kt
+++ feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/history_of_events_fragment/ui/HistoryOfEventsFragment.kt
@@ -1,14 +1,23 @@
 package com.meetingroom.andersen.feature_landing.history_of_events_fragment.ui
 
 import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.meeringroom.ui.view.base_fragment.BaseFragment
 import com.meetingroom.andersen.feature_landing.databinding.FragmentHistoryOfEventsBinding
 import com.meetingroom.andersen.feature_landing.di.history_of_events_fragment.DaggerHistoryOfEventsFragmentComponent
+import com.meetingroom.andersen.feature_landing.history_of_events_fragment.presentation.GagForHistoryEvents
 import com.meetingroom.andersen.feature_landing.history_of_events_fragment.presentation.HistoryOfEventsFragmentViewModel
+import com.meetingroom.andersen.feature_landing.upcoming_events_fragment.presentation.GagForUpcomingEvents
+import com.meetingroom.andersen.feature_landing.upcoming_events_fragment.ui.UpcomingEventAdapter
 import javax.inject.Inject
 
 class HistoryOfEventsFragment :
     BaseFragment<FragmentHistoryOfEventsBinding>(FragmentHistoryOfEventsBinding::inflate) {
+
+    private val eventAdapter by lazy { HistoryEventAdapter() }
 
     @Inject
     lateinit var viewModel: HistoryOfEventsFragmentViewModel
@@ -18,5 +27,19 @@ class HistoryOfEventsFragment :
             .build()
             .inject(this)
         super.onAttach(context)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        eventAdapter.setData(GagForHistoryEvents().generateData(9))
+        initRecyclerView()
+    }
+    private fun initRecyclerView() {
+        with(binding) {
+            upcomingEventsRecyclerView.apply {
+                setHasFixedSize(true)
+                adapter = eventAdapter
+            }
+        }
     }
 }
