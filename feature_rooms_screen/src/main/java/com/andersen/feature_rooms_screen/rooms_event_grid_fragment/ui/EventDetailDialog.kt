57451f3c97283fa267aa0eb1869dbfb43046774f
Daria Semakina <semakina.dn@gmail.com>
2021-10-29 10:31:52 +0300

    refactor code - add apply methods

--- feature_rooms_screen/src/main/java/com/andersen/feature_rooms_screen/rooms_event_grid_fragment/ui/EventDetailDialog.kt
+++ feature_rooms_screen/src/main/java/com/andersen/feature_rooms_screen/rooms_event_grid_fragment/ui/EventDetailDialog.kt
@@ -41,15 +41,15 @@ class EventDetailDialog: BaseDialogFragment<DialogEventDetailBinding>(DialogEven
                 showCopyPrompt(it, roomEvent.userEmail)
             }
             bookerSkype.setOnClickListener {
-                if (!isSkypeClientInstalled()) {
+                if (isSkypeClientInstalled().not()) {
                     goToMarket()
                 } else {
-                    val skypeUri = Uri.parse("skype:${roomEvent.userSkype}?chat")
-                    val intent = Intent(Intent.ACTION_VIEW, skypeUri)
-                    intent.component = ComponentName(SKYPE_COMPONENT_PACKAGE_NAME, SKYPE_COMPONENT_CLASS_NAME)
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-
-                    startActivity(intent)
+                    startActivity(Intent().apply {
+                        action = Intent.ACTION_VIEW
+                        data = Uri.parse("skype:${roomEvent.userSkype}?chat")
+                        component = ComponentName(SKYPE_COMPONENT_PACKAGE_NAME, SKYPE_COMPONENT_CLASS_NAME)
+                        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    })
                 }
             }
         }
@@ -65,38 +65,32 @@ class EventDetailDialog: BaseDialogFragment<DialogEventDetailBinding>(DialogEven
         return true
     }
 
-    private fun goToMarket() {
-        val marketUri = Uri.parse(MARKET_URI)
-        val intent = Intent(Intent.ACTION_VIEW, marketUri)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivity(intent)
-    }
+    private fun goToMarket() = startActivity(Intent().apply {
+        action = Intent.ACTION_VIEW
+        data = Uri.parse(MARKET_URI)
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    })
 
-    private fun showCopyPrompt(incomingView: View?, text: String) {
-        val popupWindow = PopupWindow(requireActivity())
+    private fun showCopyPrompt(incomingView: View?, text: String) = PopupWindow(requireActivity()).apply {
         val bindingPopup = PopoverCopyBinding.inflate(LayoutInflater.from(requireContext()))
-        with(popupWindow) {
-            contentView = bindingPopup.root
-            isOutsideTouchable = true
-            isFocusable = true
-            overlapAnchor = true
-            setBackgroundDrawable(null)
-            bindingPopup.textCopyClick.setOnClickListener {
-                saveTextToRAM(text)
-                dismiss()
-            }
+        contentView = bindingPopup.root
+        isOutsideTouchable = true
+        isFocusable = true
+        overlapAnchor = true
+        setBackgroundDrawable(null)
+        bindingPopup.textCopyClick.setOnClickListener {
+            saveTextToRAM(text)
+            dismiss()
         }
-
-        popupWindow.showAsDropDown(
+        showAsDropDown(
             incomingView,
             incomingView!!.width / 2,
             -incomingView.height * 2
         )
     }
 
-    private fun saveTextToRAM(text: String) {
-        val clipboardManager = requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-        clipboardManager.setPrimaryClip(ClipData.newPlainText(COPIED_TEXT_LABEL, text))
+    private fun saveTextToRAM(text: String) = (requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).apply {
+            setPrimaryClip(ClipData.newPlainText(COPIED_TEXT_LABEL, text))
     }
 
     companion object {
