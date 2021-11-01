395ae9a951668063edc54087002257a461581a34
DashaVan <43934226+DashaVan@users.noreply.github.com>
2021-11-01 16:28:46 +0300

    created ui for one_week_calendar (fragment_rooms)

--- feature_rooms_screen/src/main/java/com/andersen/feature_rooms_screen/rooms_event_grid_fragment/ui/RoomsEventGridFragment.kt
+++ feature_rooms_screen/src/main/java/com/andersen/feature_rooms_screen/rooms_event_grid_fragment/ui/RoomsEventGridFragment.kt
@@ -8,12 +8,14 @@ import com.meetingroom.andersen.feature_rooms_screen.databinding.FragmentRoomsBi
 import android.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.andersen.feature_rooms_screen.rooms_event_grid_fragment.model.RoomEvent
+import com.prolificinteractive.materialcalendarview.CalendarDay
 
 class RoomsEventGridFragment: BaseFragment<FragmentRoomsBinding>(FragmentRoomsBinding::inflate) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar()
+        initCalendar()
     }
 
     private fun initToolbar() {
@@ -25,5 +27,11 @@ class RoomsEventGridFragment: BaseFragment<FragmentRoomsBinding>(FragmentRoomsBi
     private fun showDetailsDialog(roomEvent: RoomEvent) {
         val action = RoomsEventGridFragmentDirections.actionRoomsFragmentToEventDetailDialog(roomEvent)
         findNavController().navigate(action)
+    }
+
+    private fun initCalendar() {
+        with(binding) {
+            oneWeekCalendar.setDateSelected(CalendarDay.today(), true)
+        }
     }
 }
