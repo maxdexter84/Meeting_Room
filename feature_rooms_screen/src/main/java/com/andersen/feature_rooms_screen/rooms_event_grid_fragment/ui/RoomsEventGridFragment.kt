19268d012b0a4ca3ced5b7297f66f39ad9275766
DashaVan <43934226+DashaVan@users.noreply.github.com>
2021-11-03 15:05:06 +0300

    added Decorator for one_week_calendar, created selectedDate

--- feature_rooms_screen/src/main/java/com/andersen/feature_rooms_screen/rooms_event_grid_fragment/ui/RoomsEventGridFragment.kt
+++ feature_rooms_screen/src/main/java/com/andersen/feature_rooms_screen/rooms_event_grid_fragment/ui/RoomsEventGridFragment.kt
@@ -1,21 +1,24 @@
 package com.andersen.feature_rooms_screen.rooms_event_grid_fragment.ui
 
-import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.andersen.feature_rooms_screen.rooms_event_grid_fragment.model.RoomEvent
 import com.meeringroom.ui.view.base_classes.BaseFragment
 import com.meetingroom.andersen.feature_rooms_screen.R
 import com.meetingroom.andersen.feature_rooms_screen.databinding.FragmentRoomsBinding
 import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
 import java.util.*
+
 
 class RoomsEventGridFragment: BaseFragment<FragmentRoomsBinding>(FragmentRoomsBinding::inflate) {
 
-    private var selectedDate: String? = null
+    private var selectedDateForGrid: String? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,42 +38,58 @@ class RoomsEventGridFragment: BaseFragment<FragmentRoomsBinding>(FragmentRoomsBi
         findNavController().navigate(action)
     }
 
-    @SuppressLint("NewApi")
     private fun initCalendar() {
         with(binding.oneWeekCalendar) {
             setDateSelected(CalendarDay.today(), true)
+            setCurrentDateColor()
             setOnDateChangedListener { widget, date, selected ->
-                Toast.makeText(requireContext(), "click $date", Toast.LENGTH_SHORT).show()
-                selectedDate = date
+                selectedDateForGrid = "${date.year}-${date.month}-${date.day}"
+                Toast.makeText(requireContext(), "selected $selectedDateForGrid", Toast.LENGTH_SHORT).show()
             }
             setOnTitleClickListener {
                 showDatePickerDialog()
             }
+
         }
     }
 
-    @SuppressLint("NewApi")
     private fun showDatePickerDialog() {
         val calendar = Calendar.getInstance()
         val year = calendar.get(Calendar.YEAR)
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val datePicker = DatePickerDialog(requireContext(),
-            DatePickerDialog.OnDateSetListener { view, year1, monthOfYear, dayOfMonth ->
-                selectedDate = "CalendarDay{$year1-${monthOfYear + 1}-$dayOfMonth}"
+        DatePickerDialog(
+            requireContext(),
+            DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+                selectedDateForGrid = "$year-${monthOfYear + 1}-$dayOfMonth"
+                Toast.makeText(requireContext(), "selected $selectedDateForGrid", Toast.LENGTH_SHORT).show()
                 val selDate = binding.oneWeekCalendar.selectedDate
-                binding.oneWeekCalendar.setDateSelected(selDate, false)
-                binding.oneWeekCalendar.setDateSelected(
-                    CalendarDay.from(
-                        year,
-                        monthOfYear + 1,
-                        dayOfMonth
-                    ), true
-                )
-                Toast.makeText(requireContext(), "$selectedDate", Toast.LENGTH_SHORT).show()
+                val selectedDayForCalendar = CalendarDay.from(year, monthOfYear + 1, dayOfMonth)
+                with(binding.oneWeekCalendar) {
+                    setDateSelected(selDate, false)
+                    setCurrentDate(selectedDayForCalendar, true)
+                    setDateSelected(selectedDayForCalendar, true)
+                }
             }, year, month, day
-        )
-        datePicker.show()
+        ).apply {
+            datePicker.minDate = System.currentTimeMillis()
+            show()
+        }
+    }
+
+    private fun setCurrentDateColor() {
+        binding.oneWeekCalendar.addDecorator(object : DayViewDecorator {
+            override fun shouldDecorate(day: CalendarDay): Boolean {
+                return day == CalendarDay.today()
+            }
+
+            override fun decorate(view: DayViewFacade) {
+                ContextCompat.getDrawable(
+                    requireContext(),
+                    R.drawable.selector_calendar
+                )?.let { view.setBackgroundDrawable(it) }
+            }
+        })
     }
 }
