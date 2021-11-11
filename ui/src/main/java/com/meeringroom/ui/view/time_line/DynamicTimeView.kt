64b03f37a07be720385bb4f5fbf23e2c6c862d62
Daria Semakina <semakina.dn@gmail.com>
2021-11-11 18:03:35 +0300

    set paddings to dynamic time items

--- ui/src/main/java/com/meeringroom/ui/view/time_line/DynamicTimeView.kt
+++ ui/src/main/java/com/meeringroom/ui/view/time_line/DynamicTimeView.kt
@@ -3,14 +3,12 @@ package com.meeringroom.ui.view.time_line
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.core_module.utils.timeToString
 import com.meetingroom.ui.R
 import com.meetingroom.ui.databinding.ViewDynamicTimeItemBinding
-import java.lang.Math.abs
 import java.time.LocalTime
 
 
@@ -23,7 +21,7 @@ class DynamicTimeView @JvmOverloads constructor(
 
     val binding = ViewDynamicTimeItemBinding.inflate(LayoutInflater.from(context), this, true)
     private val viewHeight = resources.getDimensionPixelSize(R.dimen.dimens_34_dp)
-    private val minSpace = resources.getDimensionPixelSize(R.dimen.dimens_20_dp)
+    private val minSpace = resources.getDimensionPixelSize(R.dimen.dimens_4_dp)
     private var startTimePadding = 0
     private var endTimePadding = 0
 
@@ -31,37 +29,16 @@ class DynamicTimeView @JvmOverloads constructor(
         set(value) {
             field = value
             if (value != null) {
-                with(binding.startTimeTextView) {
-                    text = value.timeToString(TIME_FORMAT)
-                    when {
-                        value.minute < MIN_MINUTE_WITH_PADDING -> {
-                            startTimePadding = 0
-                        }
-                        value.minute in MIN_MINUTE_WITH_PADDING..MAX_MINUTE_WITH_PADDING -> {
-                            startTimePadding = viewHeight * value.minute / MINUTES_IN_HOUR
-                        }
-                        value.minute > MAX_MINUTE_WITH_PADDING -> {
-                            startTimePadding = viewHeight
-                        }
-                    }
+                binding.startTimeTextView.text = value.timeToString(TIME_FORMAT)
+                when {
+                    value.minute < MIN_MINUTE_WITH_PADDING -> startTimePadding = 0
+                    value.minute in MIN_MINUTE_WITH_PADDING..MAX_MINUTE_WITH_PADDING ->
+                        startTimePadding = viewHeight * value.minute / MINUTES_IN_HOUR
+                    value.minute > MAX_MINUTE_WITH_PADDING -> startTimePadding = viewHeight
                 }
-                if (kotlin.math.abs(startTimePadding - endTimePadding) > minSpace) {
-                    if (startTimePadding < minSpace) {
-                        endTimePadding + minSpace
-                    } else if (endTimePadding > viewHeight - minSpace) {
-                        startTimePadding - minSpace
-                    } else {
-                        endTimePadding + minSpace / 2
-                        startTimePadding - minSpace / 2
-                    }
-                }
-                with (binding.startTimeTextView){
-                    setPadding(paddingStart, startTimePadding, paddingEnd, 0)
-                }
-                with (binding.endTimeTextView){
-                    setPadding(paddingStart, endTimePadding, paddingEnd, 0)
-                }
-
+                checkSpace()
+                setPadding(startTimePadding, binding.startTimeTextView)
+                setPadding(endTimePadding, binding.endTimeTextView)
             } else {
                 binding.startTimeTextView.text = ""
             }
@@ -71,54 +48,35 @@ class DynamicTimeView @JvmOverloads constructor(
         set(value) {
             field = value
             if (value != null) {
-                with (binding.endTimeTextView) {
-                    text = value.timeToString(TIME_FORMAT)
-                    when {
-                        value.minute < MIN_MINUTE_WITH_PADDING -> {
-                            endTimePadding = 0
-                        }
-                        value.minute in MIN_MINUTE_WITH_PADDING..MAX_MINUTE_WITH_PADDING -> {
-                            endTimePadding = viewHeight * value.minute / MINUTES_IN_HOUR
-
-                        }
-                        value.minute > MAX_MINUTE_WITH_PADDING -> {
-                            endTimePadding = viewHeight
-                        }
-                    }
-                    if (kotlin.math.abs(startTimePadding - endTimePadding) > minSpace) {
-                        if (startTimePadding < minSpace) {
-                            endTimePadding + minSpace
-                        } else if (endTimePadding > viewHeight - minSpace) {
-                            startTimePadding - minSpace
-                        } else {
-                            endTimePadding + minSpace / 2
-                            startTimePadding - minSpace / 2
-                        }
-                    }
-                    with (binding.startTimeTextView){
-                        setPadding(paddingStart, startTimePadding, paddingEnd, 0)
-                    }
-                    with (binding.endTimeTextView){
-                        setPadding(paddingStart, endTimePadding, paddingEnd, 0)
-                    }
+                binding.endTimeTextView.text = value.timeToString(TIME_FORMAT)
+                when {
+                    value.minute < MIN_MINUTE_WITH_PADDING -> endTimePadding = 0
+                    value.minute in MIN_MINUTE_WITH_PADDING..MAX_MINUTE_WITH_PADDING ->
+                        endTimePadding = viewHeight * value.minute / MINUTES_IN_HOUR
+                    value.minute > MAX_MINUTE_WITH_PADDING -> endTimePadding = viewHeight
                 }
+                checkSpace()
+                setPadding(startTimePadding, binding.startTimeTextView)
+                setPadding(endTimePadding, binding.endTimeTextView)
             } else {
                 binding.endTimeTextView.text = ""
             }
         }
 
-    private fun setPadding(minute: Int, view: View) {
+    private fun setPadding(paddingTop: Int, view: View) {
         with(view) {
+            setPadding(paddingStart, paddingTop, paddingEnd, paddingBottom)
+        }
+    }
+
+    private fun checkSpace() {
+        if (kotlin.math.abs(startTimePadding - endTimePadding) > minSpace) {
             when {
-                minute < MIN_MINUTE_WITH_PADDING -> {
-                    setPadding(view.paddingStart, 0, paddingEnd, 0)
-                }
-                minute in MIN_MINUTE_WITH_PADDING..MAX_MINUTE_WITH_PADDING -> {
-                    val paddingTop = viewHeight * minute / MINUTES_IN_HOUR
-                    setPadding(paddingStart, paddingTop, paddingEnd, 0)
-                }
-                minute > MAX_MINUTE_WITH_PADDING -> {
-                    setPadding(paddingStart, viewHeight, paddingEnd, 0)
+                startTimePadding < minSpace -> endTimePadding += minSpace
+                endTimePadding > viewHeight - minSpace -> startTimePadding -= minSpace
+                else -> {
+                    endTimePadding += minSpace / 2
+                    startTimePadding -= minSpace / 2
                 }
             }
         }
