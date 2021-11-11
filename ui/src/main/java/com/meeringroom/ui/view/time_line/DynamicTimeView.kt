a25f53d2185306663ad10ff7f41e34d622d34554
Daria Semakina <semakina.dn@gmail.com>
2021-11-11 09:57:39 +0300

    fix resetting time

--- ui/src/main/java/com/meeringroom/ui/view/time_line/DynamicTimeView.kt
+++ ui/src/main/java/com/meeringroom/ui/view/time_line/DynamicTimeView.kt
@@ -7,6 +7,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.core_module.utils.timeToString
+import com.meetingroom.ui.R
 import com.meetingroom.ui.databinding.ViewDynamicTimeItemBinding
 import java.time.LocalTime
 
@@ -17,18 +18,19 @@ class DynamicTimeView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
+
     val binding = ViewDynamicTimeItemBinding.inflate(LayoutInflater.from(context), this, true)
+    private val viewHeight = resources.getDimensionPixelSize(R.dimen.dimens_38_dp)
 
     var startTime: LocalTime? = null
         set(value) {
             field = value
             if (value != null) {
-                binding.startTimeTextView.text = value.timeToString(TIME_FORMAT)
-                val params = LayoutParams(binding.startTimeTextView.layoutParams)
-                params.topMargin = height * MINUTES_IN_HOUR / value.minute
-                Log.d("TIMELINE", " set Margin to start time ${params.topMargin}")
-                binding.startTimeTextView.layoutParams = params
-                binding.startTimeTextView.requestLayout()
+                with(binding.startTimeTextView) {
+                    text = value.timeToString(TIME_FORMAT)
+                    val paddingTop = viewHeight * value.minute / MINUTES_IN_HOUR
+                    setPadding(paddingStart, paddingTop, paddingEnd, paddingBottom)
+                }
             } else {
                 binding.startTimeTextView.text = ""
             }
@@ -38,13 +40,11 @@ class DynamicTimeView @JvmOverloads constructor(
         set(value) {
             field = value
             if (value != null) {
-
-                binding.endTimeTextView.text = value.timeToString(TIME_FORMAT)
-                val params = LayoutParams(binding.endTimeTextView.layoutParams)
-                params.topMargin = height * MINUTES_IN_HOUR / value.minute
-                Log.d("TIMELINE", " set Margin to end time ${params.topMargin}")
-                binding.endTimeTextView.layoutParams = params
-                binding.endTimeTextView.requestLayout()
+                with (binding.endTimeTextView) {
+                    text = value.timeToString(TIME_FORMAT)
+                    val paddingTop = viewHeight * value.minute / MINUTES_IN_HOUR
+                    setPadding(paddingStart, paddingTop, paddingEnd, paddingBottom)
+                }
             } else {
                 binding.endTimeTextView.text = ""
             }
