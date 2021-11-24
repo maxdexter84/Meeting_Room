b42d75cb0306da0ee6eb5b2660cc26b5aa0d4ccb
Maksim Sazonov <m.sazonov@andersenlab.com>
2021-11-24 10:08:27 +0000

    MR-457 Create_custom_view_for_event_rectangle

---
+++ ui/src/main/java/com/meeringroom/ui/view/indicator_view/Square.kt
@@ -0,0 +1,11 @@
+package com.meeringroom.ui.view.indicator_view
+
+import android.graphics.Paint
+
+data class Square(
+    val x: Float = 0f,
+    val y: Float = 0f,
+    val color: Paint = Paint(),
+    val width: Float = 0f,
+    val height: Float = 0f
+)
