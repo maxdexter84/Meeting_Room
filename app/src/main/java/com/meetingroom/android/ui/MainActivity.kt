f803d175d94c7d0e39f1ac94f7652fdca07dcf45
maxdexter <maxdexter@bk.ru>
2021-11-25 18:02:48 +0300

    MR-357_Tapping 'Location settings' Pull-down menu item in the pull-down menu (My Space Tab screen) doesn't display Select location screen

--- app/src/main/java/com/meetingroom/android/ui/MainActivity.kt
+++ app/src/main/java/com/meetingroom/android/ui/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.meetingroom.android.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.meetingroom.android.R
+import com.meetingroom.android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+        binding.bottomNavView.mainBottomNavigationView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val uri =
+                Uri.parse(resources.getString(com.meetingroom.ui.R.string.deeplink_uri_set_locations_screen))
+            if (destination.hasDeepLink(uri)) {
+                binding.bottomNavView.mainBottomNavigationView.visibility = View.INVISIBLE
+            } else binding.bottomNavView.mainBottomNavigationView.visibility = View.VISIBLE
+        }
+        navController.currentDestination
 
     }
 }
