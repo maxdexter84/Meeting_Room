cc22e2444823267dba610d0042ae9d69014d39ac
maxdexter <maxdexter@bk.ru>
2021-11-26 13:26:15 +0300

    MR-357_Tapping 'Location settings' Pull-down menu item in the pull-down menu (My Space Tab screen) doesn't display Select location screen
    refactoring

--- app/src/main/java/com/meetingroom/android/ui/MainActivity.kt
+++ app/src/main/java/com/meetingroom/android/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.meetingroom.android.R
@@ -20,16 +21,24 @@ class MainActivity : AppCompatActivity() {
         navController =
             (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
         binding.bottomNavView.mainBottomNavigationView.setupWithNavController(navController)
+        destinationListener(binding)
+
+    }
+
+    private fun destinationListener(binding: ActivityMainBinding) {
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            if (destination.hasDeepLink(Uri.parse(resources.getString(R.string.deeplink_uri_set_locations_screen))) ||
-                destination.hasDeepLink(
-                    Uri.parse(resources.getString(R.string.deeplink_uri_login_screen))
-                )
-            ) {
+            if (checkDestination(destination)) {
                 binding.bottomNavView.mainBottomNavigationView.visibility = View.INVISIBLE
             } else binding.bottomNavView.mainBottomNavigationView.visibility = View.VISIBLE
         }
+    }
 
+    private fun checkDestination(destination: NavDestination): Boolean {
+        return when {
+            destination.hasDeepLink(Uri.parse(resources.getString(R.string.deeplink_uri_set_locations_screen))) ||
+                    destination.hasDeepLink(Uri.parse(resources.getString(R.string.deeplink_uri_login_screen))) -> true
+            else -> false
+        }
     }
 
 }
