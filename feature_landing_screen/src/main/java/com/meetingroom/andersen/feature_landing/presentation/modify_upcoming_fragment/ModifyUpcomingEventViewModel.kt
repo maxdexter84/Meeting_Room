7ccd2a7e0f974bcfca7737c44d883c644773029d
Dasha Vaniukova <dashavan34@gmail.com>
2022-01-25 16:40:55 +0300

    MR-648 fixed bug after resolved conflicts with MR

--- feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/presentation/modify_upcoming_fragment/ModifyUpcomingEventViewModel.kt
+++ feature_landing_screen/src/main/java/com/meetingroom/andersen/feature_landing/presentation/modify_upcoming_fragment/ModifyUpcomingEventViewModel.kt
@@ -37,7 +37,6 @@ class ModifyUpcomingEventViewModel @Inject constructor(
         viewModelScope.launch {
             try {
                 _mutableLoadingState.emit(State.Loading)
-                roomsEventRepository.deleteUpcomingEvent(eventId)
                 val role = userDataPrefHelper.getUserRole().toString()
                 if(role == ROLE_ADMIN){
                     roomsEventRepository.deleteUpcomingEventForAdmin(eventId)
